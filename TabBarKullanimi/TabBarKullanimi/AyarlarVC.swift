d5dc3b95d090c9a1fb7833fe2c2ad2fa727b10b2
İsmail Kocaoğlu <ismailkocaogluu.27@gmail.com>
2024-01-18 22:39:18 +0300

    TabbarKullanimi

---
+++ TabBarKullanimi/TabBarKullanimi/AyarlarVC.swift
@@ -0,0 +1,19 @@
+//
+//  AyarlarVC.swift
+//  TabBarKullanimi
+//
+//  Created by İsmail Kocaoglu on 12.01.2024.
+//
+
+import UIKit
+
+class AyarlarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
