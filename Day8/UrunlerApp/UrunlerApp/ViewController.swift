afd95b62035318c37f1f641e12f5ab1cf5c0149b
İsmail Kocaoğlu <ismailkocaogluu.27@gmail.com>
2024-01-20 18:22:05 +0300

    Created UrunlerApp

---
+++ Day8/UrunlerApp/UrunlerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrunlerApp
+//
+//  Created by İsmail Kocaoglu on 20.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
