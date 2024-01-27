e42704dd485f3ee87599efb24969a0d5b8485cbd
İsmail Kocaoğlu <ismailkocaogluu.27@gmail.com>
2024-01-27 15:12:41 +0300

    how we can use Kingfisher

---
+++ Day12/KingfisherKullanimi/KingfisherKullanimi/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  KingfisherKullanimi
+//
+//  Created by İsmail Kocaoglu on 27.01.2024.
+//
+
+import UIKit
+import Kingfisher
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    resimGoster(resimAdi: "kofte.png")
+    }
+
+    @IBAction func buttonResim1(_ sender: Any) {
+        resimGoster(resimAdi: "ayran.png")
+    }
+    
+    @IBAction func buttonResim2(_ sender: Any) {
+        resimGoster(resimAdi: "su.png")
+    }
+    
+    
+    func resimGoster(resimAdi:String) {
+        if let url = URL(string: "http://kasimadalan.pe.hu/yemekler/resimler/\(resimAdi)"){
+            DispatchQueue.main.async{
+                self.imageView.kf.setImage(with:url)
+                
+            }
+        }
+    }
+    
+    
+    
+}
+
